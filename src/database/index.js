2b721d3c08c3084beda85d4e4fb01fd907b054ad
codeecoffee <fellipefl7@msn.com>
2020-06-21 19:53:06 -0300

    finished DB relation

--- src/database/index.js
+++ src/database/index.js
@@ -2,8 +2,9 @@
 import Sequelize from 'sequelize'
 import DBconfig from '../config/database'
 import User from '../app/models/User'
+import File from '../app/models/File'
 
-const models = [User]
+const models = [User, File]
 class Database{
   constructor(){
     this.init()
@@ -11,7 +12,9 @@ class Database{
   init(){
     this.connection = new Sequelize(DBconfig)
 
-    models.map(model=> model.init(this.connection))
+    models
+    .map(model=> model.init(this.connection))
+    .map(model=> model.associate && model.associate(this.connection.models))
   }
 }
 
