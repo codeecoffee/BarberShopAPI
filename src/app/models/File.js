5c105631e08020d6eaaf598d20fd49d95c091958
codeecoffee <fellipefl7@msn.com>
2020-06-22 15:12:56 -0300

    added Providers and changed file specs

--- src/app/models/File.js
+++ src/app/models/File.js
@@ -6,6 +6,13 @@ class File extends Model {
       {
         name: Sequelize.STRING,
         path: Sequelize.STRING,
+        url:{
+          type: Sequelize.VIRTUAL,
+          get(){
+            return `http://localhost:3333/files/${this.path}`
+          }
+
+        }
       }, 
       {
       sequelize,
--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import multerConfig from './config/multer'
 import UserController from './app/controllers/UserController'
 import SessionController from './app/controllers/SessionController'
 import FileController from './app/controllers/FileController'
+import ProvidersController from './app/controllers/ProvidersController'
 
 import authMiddleware from './app/middleware/auth'
 
@@ -18,6 +19,8 @@ routes.post('/sessions', SessionController.store)
 routes.use(authMiddleware)
 
 routes.put('/users', UserController.update)
+
+routes.get('/providers',ProvidersController.index)
 
 routes.post('/files', upload.single('file'), FileController.store )
 
